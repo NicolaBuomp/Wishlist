5ab1329cdf4a743856a079ad47b648d610b49796
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-27 16:25:20 +0100

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Nicola Buompane on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
