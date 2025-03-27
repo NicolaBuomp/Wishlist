e6f26421c9f2e183a68821ae6d2d71a65d33b097
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-27 17:52:32 +0100

    Final

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -6,19 +6,82 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @State private var showAddWishModal: Bool = false
+    @State private var titleTextField: String = ""
+    
+    @Query private var wishes: [Wish]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(wishes) { wish in
+                    Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                        .swipeActions {
+                            Button("Delete", role: .destructive) {
+                                modelContext.delete(wish)
+                            }
+                        }
+                }
+            } //: LIST
+            .navigationTitle("Wishlist")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        showAddWishModal.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+                if wishes.isEmpty != true {
+                    ToolbarItem(placement: .bottomBar) {
+                        Text("\(wishes.count) wish\(wishes.count > 1 ? "es" : "")")
+                    }
+                }
+            }
+            .alert("Create a new wish", isPresented: $showAddWishModal) {
+                TextField("Enter a wish", text: $titleTextField)
+                Button {
+                    modelContext.insert(Wish(title: titleTextField))
+                    titleTextField = ""
+                } label: {
+                    Text("Add")
+                }
+                Button {} label: {
+                    Text("Close")
+                }
+            }
+            .overlay {
+                if wishes.isEmpty {
+                    ContentUnavailableView("My Wishlist is empty.", systemImage: "heart.circle", description: Text("Add your first wish!"))
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Wish.self, inMemory: true)
+}
+
+#Preview("List with simple data") {
+    do {
+        let container = try ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+        let context = container.mainContext
+        context.insert(Wish(title: "Test Wish 1"))
+        context.insert(Wish(title: "Test Wish 2"))
+        context.insert(Wish(title: "Test Wish 3"))
+        context.insert(Wish(title: "Test Wish 4"))
+
+        return ContentView()
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to load preview: \(error.localizedDescription)")
+    }
 }
